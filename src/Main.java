7149a14914b4195807fbeaba694e41a0b5600302
Akina KOMINE <e165752@ie.u-ryukyu.ac.jp>
2016-12-12 19:47:29 +0900

    step3(parseDouble() postscript)

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = null;
+            String str = "3．14";
+            double value = Double.parseDouble(str);
             System.out.println("strの長さ = " + str.length());
 
         } catch (NullPointerException e) {
