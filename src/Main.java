b990f714a7732845531d8e7fedaef239163b6b64
Akina KOMINE <e165752@ie.u-ryukyu.ac.jp>
2016-12-12 17:43:38 +0900

    error occurred

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by komineakina on 2016/12/12.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println("strの長さ = " + str.length());
+    }
+}
