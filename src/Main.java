3106b79d04b326212fc75c60bea211cbc80a1375
Akina KOMINE <e165752@ie.u-ryukyu.ac.jp>
2016-12-12 19:18:56 +0900

    step2(try-catch)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@
  * Created by komineakina on 2016/12/12.
  */
 public class Main {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println("strの長さ = " + str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println("strの長さ = " + str.length());
+
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+            System.out.println("NullPointerExceptionが発生");
+            e.printStackTrace();
+        }
     }
 }
